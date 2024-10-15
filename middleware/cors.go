b6c3a2b128c427e7012c34fb9384457a1011cf45
Anderson Gaitolini <gaitolini@gmail.com>
2024-10-15 13:16:25 -0300

    Update cors.go

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,16 +6,17 @@ import (
 )
 
 // Middleware para adicionar cabeçalhos CORS manualmente
-func CorsMiddleware(next http.Handler) http.Handler {
+func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("CORS Middleware ativado para %s %s", r.Method, r.URL.Path)
 
+		// Definir os cabeçalhos de CORS
 		w.Header().Set("Access-Control-Allow-Origin", "https://eleicoesvirtual.web.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Tratamento de requisições preflight
+		// Se a requisição for OPTIONS, respondemos imediatamente
 		if r.Method == http.MethodOptions {
 			log.Printf("Requisição preflight OPTIONS recebida e tratada")
 			w.WriteHeader(http.StatusOK)
