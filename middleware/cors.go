36281672cd48fd76c90d14c9c35eb6ce0214839f
Anderson Gaitolini <gaitolini@gmail.com>
2024-10-20 20:29:08 -0300

    Update cors.go

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,26 +11,27 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		log.Printf("CORS Middleware ativado para %s %s", r.Method, r.URL.Path)
 
 		origin := r.Header.Get("Origin")
-		log.Printf("Origin: %s", origin)
-		// Definir os cabeçalhos CORS dependendo da origem da requisição
-		if origin == "http://localhost:3000" || origin == "https://eleicoesvirtual.web.app" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		} else {
-			w.Header().Set("Access-Control-Allow-Origin", "https://eleicoesvirtual.web.app") // Origem padrão (pode ser alterado)
+		if origin == "" {
+			log.Printf("Origin não encontrado. Usando valor padrão.")
+			origin = "https://eleicoesvirtual.web.app" // Origem padrão
 		}
 
+		log.Printf("Origin: %s", origin)
+
+		// Configurar CORS
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Responder imediatamente se for uma requisição preflight OPTIONS
+		// Responder imediatamente se for uma requisição preflight (OPTIONS)
 		if r.Method == http.MethodOptions {
 			log.Printf("Recebendo requisição OPTIONS para %s", r.URL.Path)
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		// Passar para o próximo middleware se não for OPTIONS
+		// Passar para o próximo middleware
 		next.ServeHTTP(w, r)
 	})
 }
