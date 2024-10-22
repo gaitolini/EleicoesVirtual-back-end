1f1ab7e1b3fe36ea20aeef80f099a1a4afebe808
Anderson Gaitolini <gaitolini@gmail.com>
2024-10-22 14:15:24 -0300

    Update cors.go

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,23 +10,14 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("CORS Middleware ativado para %s %s", r.Method, r.URL.Path)
 
-		origin := r.Header.Get("Origin")
-		if origin == "" {
-			log.Printf("Origin não encontrado. Usando valor padrão.")
-			origin = "https://eleicoesvirtual.web.app" // Origem padrão
-		}
-
-		log.Printf("Origin: %s", origin)
-
-		// Configurar CORS
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+		// Permitir qualquer origem ou uma lista de origens permitidas
+		w.Header().Set("Access-Control-Allow-Origin", "https://eleicoesvirtual.web.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// Responder imediatamente se for uma requisição preflight (OPTIONS)
+		// Verifica se é uma requisição preflight (OPTIONS)
 		if r.Method == http.MethodOptions {
-			log.Printf("Recebendo requisição OPTIONS para %s", r.URL.Path)
 			w.WriteHeader(http.StatusOK)
 			return
 		}
